c7c23dd3e95a465d916e7cbd21d9459b7f070b95
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-07-30 00:30:46 +0530

    Added JS file

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+var url = "https://api.funtranslations.com/translate/minion.json"
+
 var btn = document.querySelector(".btn-main");
 var txtArea = document.querySelector("#text-area");
 var outputArea = document.querySelector(".output-area")
 
-function clickFunction() {
-    console.log("Clicked")
-    console.log(txtArea.value)
-    // outputArea.innerText("I love you DAD")
+function getTranslatedURL(text) {
+    return url + "?text=" + text;
 }
 
-btn.addEventListener("click",clickFunction);
+function errorHandler(error) {
+    console.log("error occured",error)
+}
 
+function clickFunction() {
+    var inputText = txtArea.value;
+
+    fetch(getTranslatedURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated;
+        outputArea.innerText = translatedText;
+
+    })
+    .catch(errorHandler)
+};
+
+
+btn.addEventListener("click",clickFunction);
 
 
 
