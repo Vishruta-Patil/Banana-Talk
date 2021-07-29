a703a994770cd46cca18988fcea03ac5bd1431d1
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-07-29 17:03:57 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var btn = document.querySelector(".btn-main");
+var txtArea = document.querySelector("#text-area");
+var outputArea = document.querySelector(".output-area")
+
+function clickFunction() {
+    console.log("Clicked")
+    console.log(txtArea.value)
+    outputArea.innerText("I love you DAD")
+}
+
+btn.addEventListener("click",clickFunction);
+
+
+
+
+
+
+
+
+
+
+
+//  btn.onclick = function() {
+//     console.log("hjk")
+// }
+
